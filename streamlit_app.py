fb3ee659b6898fe7710337cee621d004dbae451b
MOHIT  KUMAR <32009075+beingMohit16@users.noreply.github.com>
2023-09-25 10:46:58 +0530

    added few more body line with import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title('Test = 1 ')
+streamlit.title('Test = Title ')
+
+streamlit.header('Test = Header')
+streamlit.text('Test = Text')
+streamlit.text('Test2 = Text1')
+Streamlit.text('Test3 = Text2')
