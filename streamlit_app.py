4cbe45fda309326789742487c967228a523c1686
MOHIT  KUMAR <32009075+beingMohit16@users.noreply.github.com>
2023-09-27 19:54:21 +0530

    convert json into dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,12 +24,14 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
+# convert json file format into dataframe
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+
+# display in a dataframe
 streamlit.dataframe(fruityvice_normalized)
+
+
 
 
 
